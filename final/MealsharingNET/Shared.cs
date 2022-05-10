d657d111f481b41b6ab894dd6e4e6b5f7bf03b74
Enqira <moenkira@yahoo.com>
2022-05-10 19:42:28 +0200

    update shared file

--- final/MealsharingNET/Shared.cs
+++ final/MealsharingNET/Shared.cs
@@ -1,4 +1,6 @@
 public class Shared
 {
-    public const string ConnectionString = "Server=localhost;Database=meal-sharing;Uid=root;Pwd=;convert zero datetime=true";
+    public static string ConnectionString = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb")) ?
+  "Server=localhost;Database=meal-sharing;Uid=root;Pwd=;convert zero datetime=true" :
+   Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb");
 }
